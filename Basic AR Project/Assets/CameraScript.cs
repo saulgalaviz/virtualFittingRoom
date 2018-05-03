ee5429b5d4a6deb69c3b17527a700797d28ab949
Nostrama <nostramagear@gmail.com>
2018-05-02 19:03:20 -0700

    Working on Basic AR Project

---
+++ Basic AR Project/Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    static WebCamTexture backCam;
+
+    void Start()
+    {
+        if (backCam == null)
+            backCam = new WebCamTexture();
+
+        GetComponent<Renderer>().material.mainTexture = backCam;
+
+        if (!backCam.isPlaying)
+            backCam.Play();
+
+    }
+
+    void Update()
+    {
+
+    }
+}
